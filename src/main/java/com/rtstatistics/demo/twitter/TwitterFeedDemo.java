33f4ea767b8d98f28bd8af48fc89cd24deaf9d94
James Hu <james.hu.ustc@hotmail.com>
2016-09-11 20:47:03 +1000

    finish the code

--- src/main/java/com/rtstatistics/demo/twitter/TwitterFeedDemo.java
+++ src/main/java/com/rtstatistics/demo/twitter/TwitterFeedDemo.java
@@ -3,6 +3,9 @@ package com.rtstatistics.demo.twitter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.rtstatistics.client.DataApiClient;
+
+import net.sf.jabb.util.text.DurationFormatter;
 import twitter4j.StallWarning;
 import twitter4j.Status;
 import twitter4j.StatusDeletionNotice;
@@ -13,13 +16,18 @@ import twitter4j.TwitterStreamFactory;
 public class TwitterFeedDemo implements StatusListener {
 	static private final Logger logger = LoggerFactory.getLogger(TwitterFeedDemo.class);
 	
+	String datasetId = System.getProperty("twitter.feed.demo.dataset.id", System.getenv("twitter.feed.demo.dataset.id"));
+	String datasetSendKey = System.getProperty("twitter.feed.demo.dataset.sendkey", System.getenv("twitter.feed.demo.dataset.sendkey"));
+	DataApiClient apiClient = new DataApiClient(datasetId, datasetSendKey, null);
+	
 	public static void main(String[] args) {
+		
 		TwitterStream twitterStream = new TwitterStreamFactory().getInstance();
 		twitterStream.addListener(new TwitterFeedDemo());
 		
 		logger.debug("Start to receive sampled feeds from twitter.com");
 		twitterStream.sample();
-		logger.info("Successfully started");
+		logger.info("Successfully started. Press Ctrl-C to stop.");
 		
 		
 		Runtime.getRuntime().addShutdownHook(new Thread(){
@@ -34,12 +42,16 @@ public class TwitterFeedDemo implements StatusListener {
 	
 	@Override
 	public void onStatus(Status status) {
-		logger.debug("Received: {}", status);
+		//logger.debug("Received: {}", status);
+		long startTime = System.currentTimeMillis();
+		apiClient.send(status);
+		//logger.debug("Sent: {}", status);
+		logger.debug("Sent. ID={}, duration: {}", status.getId(), DurationFormatter.formatSince(startTime));
 	}
 
 	@Override
 	public void onException(Exception ex) {
-		logger.error("Happened when receiving from twitter.com", ex);
+		logger.error("Error happened when receiving from twitter.com", ex);
 		
 	}
 
